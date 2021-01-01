935317576f45a43e1d8ddf284b1d8a4bc7f8e0ce
RoseMagura <magurarm@guilford.edu>
2021-01-01 15:07:18 -0500

    Edit: Finish adding tests

--- counting_cards.js
+++ counting_cards.js
@@ -20,4 +20,8 @@ function cc(card) {
 
 // cc(2); cc(3); cc(4); cc(5); console.log(cc(6)); // 5 Bet
 // cc(7); cc(8); console.log(cc(9)); // 0 Hold
-cc(10); cc('J'); cc('Q'); cc('K'); console.log(cc('A')); // -5 Hold
+// cc(10); cc('J'); cc('Q'); cc('K'); console.log(cc('A')); // -5 Hold
+// cc(3); cc(7); cc('Q'); cc(8); console.log(cc('A')); // -1 Hold
+// cc(2); cc('J'); cc(9); cc(2); console.log(cc(7)); // 1 Bet 
+// cc(2); cc(2); console.log(cc(10)); // 1 Bet
+cc(3); cc(2); cc('A'); cc(10); console.log(cc('K')); // -1 Hold
